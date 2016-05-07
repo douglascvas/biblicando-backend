bc25069cdce6602c845a19ba0ba6154287196bb8
Douglas Carvalho Vasconcelos <douglascvas@gmail.com>
2016-05-07 23:25:26 +0200

    Base code.

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 const gulp = require('gulp');
 const del = require('del');
 const typescript = require('gulp-typescript');
-const tscConfig = require('./tsconfig.json');
 const sourcemaps = require('gulp-sourcemaps');
 const tslint = require('gulp-tslint');
 
@@ -12,16 +11,8 @@ gulp.task('clean', function () {
 // TypeScript compile
 var tsProject = typescript.createProject('tsconfig.json');
 gulp.task('compile', function () {
-  // return tsResult = tsProject
-  //   .src('src/main/**/*.ts')
-  //   .pipe(sourcemaps.init())
-  //   .pipe(typescript(tsProject))
-  //   .pipe(sourcemaps.write('.'))
-  //   .pipe(gulp.dest('build'));
-
-  var sourceTsFiles = ['src/main/**/*.ts',                //path to typescript files
-    'typings/main/**/*.ts', 'typings/main.d.ts']; //reference to library .d.ts files
-
+  var sourceTsFiles = ['src/main/**/*.ts',
+    'typings/main/**/*.ts', 'typings/main.d.ts'];
 
   var tsResult = gulp.src(sourceTsFiles)
     .pipe(sourcemaps.init())
@@ -32,21 +23,6 @@ gulp.task('compile', function () {
     .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest('build'));
 });
-
-// gulp.task('compile', function () {
-//   var sourceTsFiles = [config.allTypeScript,                //path to typescript files
-//     config.libraryTypeScriptDefinitions]; //reference to library .d.ts files
-//
-//
-//   var tsResult = gulp.src(sourceTsFiles)
-//     .pipe(sourcemaps.init())
-//     .pipe(tsc(tsProject));
-//
-//   tsResult.dts.pipe(gulp.dest(config.tsOutputPath));
-//   return tsResult.js
-//     .pipe(sourcemaps.write('.'))
-//     .pipe(gulp.dest(config.tsOutputPath));
-// });
 
 gulp.task('copy:resources', ['clean'], function () {
   return gulp.src(['src/main/**/*', '!src/main/**/*.ts'], {base: './src/main/'})
